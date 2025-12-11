8dfb91ae1ebd9b407b8949064c187ea6f5ad191d
SANJAY BHAGTANI <sbhagtani258@gmail.com>
2025-12-11 12:00:03 +0530

    Update to-do-list.js

--- to-do-list.js
+++ to-do-list.js
@@ -7,22 +7,22 @@ function settask() {
         alert("plzzz added date and task !!");
     } else {
         let li = document.createElement("li");
-        li.style = "text-align:center; padding:2px; margin:5px; background-color:yellow; border-radius:5px;color:black;box-shadow:2px 2px 5px pink;font-size:26px;";
-        li.innerHTML ="<input type='checkbox' id='task-checkbox' onclick='complete()'>" +inputBox.value+"<button class='delete-btn' onclick='this.parentElement.remove()'><i class=\"fa-solid fa-trash-can\"></i></button>";
+        li.style = "text-align:center; padding:2px; margin:5px; background-color:skyblue; border-radius:5px;color:black;box-shadow:2px 2px 5px skyblue;font-size:26px;";
+        li.innerHTML ="<input type='checkbox' id='task-checkbox' onclick='complete(this)'>" +inputBox.value+"<button class='delete-btn' onclick='this.parentElement.remove()'><i class=\"fa-solid fa-trash-can\"></i></button>";
         tasklist.appendChild(li);
         inputBox.value = "";
     }
 }
-function complete() {
-    let checkbox = document.getElementById("task-checkbox");
-    if (checkbox.checked == true) {
-        checkbox.parentElement.style.textDecoration = "line-through";
-        checkbox.parentElement.style.backgroundColor = "lightgreen";
-    }
- else {
-        checkbox.parentElement.style.textDecoration = "none";
-        checkbox.parentElement.style.backgroundColor = "rgba(57, 155, 177, 0.6)";
-    }
+
+function complete(cb) {
+  if (cb.checked) {
+    cb.parentElement.style.textDecoration = "line-through";
+    cb.parentElement.style.backgroundColor = "lightgreen";
+    cb.parentElement.style.color = "black";
+  } else {
+    cb.parentElement.style.textDecoration = "none";
+    cb.parentElement.style.backgroundColor = "skyblue";
+  }
 }
 
 
