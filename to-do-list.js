00b32b5e223aea0710015790c4e9d9cd2c075ac8
SANJAY BHAGTANI <sbhagtani258@gmail.com>
2025-12-21 12:57:49 +0530

    Refactor settask and complete functions for clarity

--- to-do-list.js
+++ to-do-list.js
@@ -2,12 +2,12 @@ function settask() {
     let inputBox = document.getElementById("input-box");
     let tasklist = document.getElementById("task-list");
     let taskcontainer = document.querySelector(".task-container");
-    let dateInput = document.getElementById("date-input");
-    if (inputBox.value== "" || dateInput.value=="") {
-        alert("plzzz added date and task !!");
+      taskcontainer.style.display = "block";
+    if (inputBox.value== "") {
+        alert("plzzz added task first!!");
     } else {
         let li = document.createElement("li");
-        li.style = "text-align:center; padding:2px; margin:5px; background-color:skyblue; border-radius:5px;color:black;box-shadow:2px 2px 5px skyblue;font-size:26px;";
+        li.style = "text-align:center; margin:1px; background-color:rgb(87,144,255); border-radius:8px;color:black;box-shadow:2px 2px 5px black;font-weight:bold;font-size:23px;";
         li.innerHTML ="<input type='checkbox' id='task-checkbox' onclick='complete(this)'>" +inputBox.value+"<button class='delete-btn' onclick='this.parentElement.remove()'><i class=\"fa-solid fa-trash-can\"></i></button>";
         tasklist.appendChild(li);
         inputBox.value = "";
@@ -17,11 +17,12 @@ function settask() {
 function complete(cb) {
   if (cb.checked) {
     cb.parentElement.style.textDecoration = "line-through";
-    cb.parentElement.style.backgroundColor = "lightgreen";
+    cb.parentElement.style.backgroundColor = "rgba(103, 175, 94, 1)";
     cb.parentElement.style.color = "black";
   } else {
     cb.parentElement.style.textDecoration = "none";
-    cb.parentElement.style.backgroundColor = "skyblue";
+    cb.parentElement.style.backgroundColor = "rgb(87,144,255)";
+    cb.parentElement.style.color = "black";
   }
 }
 
